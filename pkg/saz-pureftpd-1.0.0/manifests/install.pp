a07478cc5c9178c6bffa7170b7c276b2b23cdb94
Saz <me@saz.sh>
2011-11-07 18:42:58 +0100

    remove build files

--- pkg/saz-pureftpd-1.0.0/manifests/install.pp
+++
@@ -1,5 +0,0 @@
-class pureftpd::install {
-    package { $pureftpd::params::package_name:
-        ensure => present,
-    }
-}
--- pkg/saz-pureftpd-1.0.0/manifests/service.pp
+++
@@ -1,9 +0,0 @@
-class pureftpd::service {
-    service { $pureftpd::params::service_name:
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => false,
-        require    => Class['pureftpd::config'],
-    }
-}
